78e1a4aaf469c29aa6ec43c1dd92c3cc8f5fd5ac
Justyna Stepniak <stepniakj@gmail.com>
2018-05-23 17:04:54 +0200

    initial commit

---
+++ 18_04_lab8_vectors/2-3VectorCount.c
@@ -0,0 +1,89 @@
+/* example8-2.c */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+void printVector(int* a, int n)
+{
+      int i;
+      for (i = 0; i < n; i++)
+      {
+            printf("%2i:  %3i\n",i, a[i]);
+      }
+}
+
+int vectorSum(int* a)
+{
+    int sum = 0;
+    int i;
+    for (i=0; i<100; i++)
+    {
+        sum += a[i];
+    }
+
+    return sum;
+}
+
+double vectorLength(int* a)
+{
+    int i, q;
+    double vlen = 0;
+    double sum = 0;
+    for (i=0; i<100; i++)
+    {
+        q = a[i] * a[i];
+        sum += q;
+    }
+
+    vlen = sqrt(sum*1.0);
+
+    return vlen;
+}
+
+void countVector(int*a, int*c)
+{
+    int i;
+    for (i=0; i<10; i++)
+    {
+        c[i] = 0;
+    }
+
+    int temp;
+    for (i=0; i<100; i++)
+    {
+        temp = a[i];
+        c[temp-1]++;
+    }
+}
+
+int main()
+{
+      int vec[100];
+      int countVec[10];
+
+      /* seeding the random number generator: */
+      srand(time(NULL));
+
+      int i;
+      for (i = 0; i < 100; i++)
+      {
+            vec[i] = rand() % 10 + 1;
+      }
+
+      printf("The vector is:\n");
+      printVector(vec, 100);
+
+      printf("The sum of the vector is %i", vectorSum(vec));
+      printf("\n");
+      printf("The length of the vector is %f", vectorLength(vec));
+
+      countVector(vec,countVec);
+
+      for(i=0; i<=10; i++)
+      {
+          printf("Number %i occurs %i times\n", i, countVec[i-1]);
+      }
+
+      return EXIT_SUCCESS;
+}
